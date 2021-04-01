ece493669ba89fd80ec0c6910107751ce447a38e
github_username <hamidreza.azarbad77@gmail.com>
2021-04-01 21:31:24 +0430

    create article data sources

---
+++ app/src/main/java/com/example/mobilenetworkproject/data/Datasource.kt
@@ -0,0 +1,11 @@
+package com.example.mobilenetworkproject.data
+
+import com.example.mobilenetworkproject.model.ArticleModel
+
+class Datasource {
+    fun loadAffirmations(): List<ArticleModel> {
+        return listOf<ArticleModel>(
+            // TODO  add articles here
+        )
+    }
+}
