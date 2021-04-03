c6ed88ad2a9f077dc4d03d13b532c9a3a20f3d20
github_username <hamidreza.azarbad77@gmail.com>
2021-04-03 23:47:19 +0430

    add articles data

--- app/src/main/java/com/example/mobilenetworkproject/data/Datasource.kt
+++ app/src/main/java/com/example/mobilenetworkproject/data/Datasource.kt
@@ -1,11 +1,13 @@
 package com.example.mobilenetworkproject.data
 
+import com.example.mobilenetworkproject.R
 import com.example.mobilenetworkproject.model.ArticleModel
 
 class Datasource {
     fun loadAffirmations(): List<ArticleModel> {
         return listOf<ArticleModel>(
-            // TODO  add articles here
+            ArticleModel(R.string.article1_title, R.string.article1_sub_title, R.string.article1_star, R.drawable.image1),
+            ArticleModel(R.string.article2_title, R.string.article2_sub_title, R.string.article2_star, R.drawable.image2)
         )
     }
 }
