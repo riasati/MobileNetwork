0e248fb79e1fbeaa49b8fc718600f6c7af995743
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2021-05-21 20:25:46 +0430

    demo of detail page

--- app/src/main/java/com/example/mobilenetworkproject/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/mobilenetworkproject/ui/slideshow/SlideshowFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -21,10 +22,18 @@ class SlideshowFragment : Fragment() {
     ): View? {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val root = inflater.inflate(R.layout.tourist_place_detail_card, container, false)
+        val imageView: ImageView = root.findViewById(R.id.tourist_place_image)
+        val textView_name: TextView = root.findViewById(R.id.tourist_place_name)
+        val textView_country: TextView = root.findViewById(R.id.tourist_place_country)
+        val textView_star: TextView = root.findViewById(R.id.tourist_place_star)
+        val textView_description: TextView = root.findViewById(R.id.tourist_place_description)
+        slideshowViewModel.touristPlacesModel.observe(viewLifecycleOwner, Observer {
+            imageView.setImageResource(it[1].imageResourceId)
+            textView_name.text = it[1].name
+            textView_country.text = it[1].country
+            textView_star.text = it[1].star.toString()
+            textView_description.text = it[1].description
         })
         return root
     }
--- app/src/main/java/com/example/mobilenetworkproject/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/example/mobilenetworkproject/ui/slideshow/SlideshowViewModel.kt
@@ -3,11 +3,15 @@ package com.example.mobilenetworkproject.ui.slideshow
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mobilenetworkproject.model.entity.TouristPlaceModel
+import com.example.mobilenetworkproject.model.repository.HomeDataRepository
 
 class SlideshowViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _touristPlacesModel: MutableLiveData<Array<TouristPlaceModel>> =
+        MutableLiveData<Array<TouristPlaceModel>>().apply {
+            value = HomeDataRepository.getTouristPlace()
+        }
+
+    val touristPlacesModel: LiveData<Array<TouristPlaceModel>> = _touristPlacesModel
 }
