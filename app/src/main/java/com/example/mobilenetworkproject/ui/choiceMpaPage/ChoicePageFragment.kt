ff62c999a953f0976453e60eeed353dbf5f5ba94
github_username <hamidreza.azarbad77@gmail.com>
2021-06-25 16:55:40 +0430

    write base of chioce map page

--- app/src/main/java/com/example/mobilenetworkproject/ui/choiceMpaPage/ChoicePageFragment.kt
+++ app/src/main/java/com/example/mobilenetworkproject/ui/choiceMpaPage/ChoicePageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,6 +23,22 @@ class ChoicePageFragment : Fragment() {
         aboutUsViewModel =
                 ViewModelProviders.of(this).get(ChoicePageViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val lacButton: Button = root.findViewById(R.id.lac_button)
+        lacButton.setOnClickListener{
+
+        }
+        val cellButton: Button = root.findViewById(R.id.cell_button)
+        cellButton.setOnClickListener{
+
+        }
+        val plmnButton: Button = root.findViewById(R.id.plmn_button)
+        plmnButton.setOnClickListener{
+
+        }
+        val tecButton: Button = root.findViewById(R.id.tec_button)
+        tecButton.setOnClickListener{
+
+        }
         return root
     }
 }
